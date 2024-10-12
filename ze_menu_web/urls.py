37c217d1e88c8d538e4f9f50b82e5a2b8df79590
Pablo Brito <pablobrith@gmail.com>
2024-10-12 20:09:30 -0300

    Listando mesas e pedidos

--- ze_menu_web/urls.py
+++ ze_menu_web/urls.py
@@ -14,14 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.contrib import admin 
+from django.urls import path, include 
 from . import views
+
+app_name = 'pedido'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('', views.index, name="index"),
 	path('login.html', views.Login.as_view(), name='login'),
 	path('painel.html', views.Painel.as_view(), name='painel'),
-	path('logout.html', views.Logout.as_view(), name='logout')
+	path('logout.html', views.Logout.as_view(), name='logout'),
+    path('pedidos/', views.PedidoListView.as_view(), name='pedido'),
+    path('pedidos/aumentar/<int:mesa_id>/<int:pedido_id>/', views.AumentarQuantidadePedido.as_view(), name='aumentar_pedido'),
+    path('pedidos/diminuir/<int:mesa_id>/<int:pedido_id>/', views.DiminuirQuantidadePedido.as_view(), name='diminuir_pedido'),
+    path('pedidos/deletar/<int:mesa_id>/<int:pedido_id>/', views.DeletarPedido.as_view(), name='deletar_pedido'),
+    path('fecharconta/<int:pk>/', views.FecharConta.as_view(), name='fechar_conta'),
 ]
