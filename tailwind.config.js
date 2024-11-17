4b17d4e8722fd1226532cb9d51fca265f5f831c9
fariaiago <111879008+fariaiago@users.noreply.github.com>
2024-11-17 20:57:40 -0300

    Sistema de toast para notificar usuário

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+	content: [
 	'./templates/*.html',
-  ],
-  theme: {
-	fontFamily: {
-		'sans': ['"Open Sans"', 'sans-serif']
+	],
+	theme: {
+		fontFamily: {
+			'sans': ['"Open Sans"', 'sans-serif']
+		},
+		extend: {
+			colors: {
+			"ze-verde1": "#14C871",
+			"ze-verde2": "#53E883",
+			"ze-sucesso": "#2CB859",
+			"ze-aviso": "#EAC648",
+			"ze-erro": "#E75858",
+			"ze-fdark": "#BFBFBF",
+			},
+			animation: {
+				fade: 'fadeOut 10s ease-in-out forwards',
+			},
+			keyframes: theme => ({
+				fadeOut: {
+					'0%': { opacity: 1 },
+					'100%': { opacity: 0, visibility: 'hidden' },
+				},
+			}),
+		},
 	},
-    extend: {
-      colors: {
-        "ze-verde1": "#14C871",
-		"ze-verde2": "#53E883",
-		"ze-sucesso": "#2CB859",
-		"ze-aviso": "#EAC648",
-		"ze-erro": "#E75858",
-		"ze-fdark": "#BFBFBF",
-      }
-    },
-  },
-  plugins: [],
+	plugins: [],
 }
 
